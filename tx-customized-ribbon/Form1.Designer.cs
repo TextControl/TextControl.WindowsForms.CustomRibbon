7531f42f33a6fa83a21364deb4774814fdad80f3
Bjoern Meyer <bjoern@textcontrol.com>
2020-01-14 15:28:25 +0100

    Initial upload

---
+++ tx-customized-ribbon/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace tx_customized_ribbon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.ribbon1 = new TXTextControl.Windows.Forms.Ribbon.Ribbon();
+            this.ribbonFormattingTab1 = new TXTextControl.Windows.Forms.Ribbon.RibbonFormattingTab();
+            this.ribbonInsertTab1 = new TXTextControl.Windows.Forms.Ribbon.RibbonInsertTab();
+            this.ribbonReportingTab1 = new TXTextControl.Windows.Forms.Ribbon.RibbonReportingTab();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.ribbon1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(25, 143);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.Ribbon = this.ribbon1;
+            this.textControl1.RulerBar = this.rulerBar2;
+            this.textControl1.Size = new System.Drawing.Size(1037, 265);
+            this.textControl1.StatusBar = this.statusBar1;
+            this.textControl1.TabIndex = 0;
+            this.textControl1.UserNames = null;
+            this.textControl1.VerticalRulerBar = this.rulerBar1;
+            // 
+            // ribbon1
+            // 
+            this.ribbon1.Controls.Add(this.ribbonFormattingTab1);
+            this.ribbon1.Controls.Add(this.ribbonInsertTab1);
+            this.ribbon1.Controls.Add(this.ribbonReportingTab1);
+            this.ribbon1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ribbon1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ribbon1.HotTrack = true;
+            this.ribbon1.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.Name = "ribbon1";
+            this.ribbon1.SelectedIndex = 3;
+            this.ribbon1.Size = new System.Drawing.Size(1062, 118);
+            this.ribbon1.TabIndex = 1;
+            this.ribbon1.Text = "ribbon1";
+            // 
+            // ribbonFormattingTab1
+            // 
+            this.ribbonFormattingTab1.Location = new System.Drawing.Point(4, 25);
+            this.ribbonFormattingTab1.Name = "ribbonFormattingTab1";
+            this.ribbonFormattingTab1.Size = new System.Drawing.Size(1054, 89);
+            this.ribbonFormattingTab1.TabIndex = 1;
+            // 
+            // ribbonInsertTab1
+            // 
+            this.ribbonInsertTab1.Location = new System.Drawing.Point(4, 25);
+            this.ribbonInsertTab1.Name = "ribbonInsertTab1";
+            this.ribbonInsertTab1.Size = new System.Drawing.Size(1054, 89);
+            this.ribbonInsertTab1.TabIndex = 2;
+            // 
+            // ribbonReportingTab1
+            // 
+            this.ribbonReportingTab1.Location = new System.Drawing.Point(4, 24);
+            this.ribbonReportingTab1.Name = "ribbonReportingTab1";
+            this.ribbonReportingTab1.Size = new System.Drawing.Size(1054, 90);
+            this.ribbonReportingTab1.TabIndex = 3;
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Location = new System.Drawing.Point(0, 118);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(1062, 25);
+            this.rulerBar2.TabIndex = 4;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 408);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(1062, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 143);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 265);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 430);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.ribbon1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ribbon1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.Windows.Forms.Ribbon.Ribbon ribbon1;
+        private TXTextControl.Windows.Forms.Ribbon.RibbonFormattingTab ribbonFormattingTab1;
+        private TXTextControl.Windows.Forms.Ribbon.RibbonInsertTab ribbonInsertTab1;
+        private TXTextControl.RulerBar rulerBar2;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private TXTextControl.Windows.Forms.Ribbon.RibbonReportingTab ribbonReportingTab1;
+    }
+}
+
---
+++ tx-customized-ribbon/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TXTextControl.DocumentServer.DataSources;
+using TXTextControl.Windows.Forms.Ribbon;
+
+namespace tx_customized_ribbon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        dynamic joData = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            joData = JsonConvert.DeserializeObject(File.ReadAllText("data.json"));
+
+            ribbonReportingTab1.DataSourceManager.LoadJson(joData.ToString());
+            ribbonReportingTab1.DataSourceManager.PossibleMergeFieldColumnsChanged += 
+                DataSourceManager_PossibleMergeFieldColumnsChanged;
+
+            //MaskDataColumnNames(joData);
+        }
+
+        private void DataSourceManager_PossibleMergeFieldColumnsChanged(object sender, EventArgs e)
+        {
+            MaskDataColumnNames(joData);
+        }
+
+        private void MaskDataColumnNames(dynamic data)
+        {
+            // flatten data object in of an array
+            if (data.GetType() == typeof(JArray))
+                data = data[0];
+
+            // find merge fields ribbon menu button
+            RibbonMenuButton ctlInsertMergeFields =
+                ribbonReportingTab1.FindItem(
+                    RibbonReportingTab.RibbonItem.TXITEM_InsertMergeField)
+                    as RibbonMenuButton;
+
+            // get the selected master table info
+            DataTableInfo dataTableInfo =
+                ribbonReportingTab1.DataSourceManager.MasterDataTableInfo;
+
+            // select token in data object
+            if (dataTableInfo.TableName != "RootTable")
+            {
+                data = data.SelectToken("$.." + dataTableInfo.TableName);
+            }
+                
+            // change the strings
+            ApplyMaskedString(ctlInsertMergeFields.DropDownItems, data);           
+        }
+
+        private void ApplyMaskedString(RibbonItemCollection ribbonItems, dynamic data)
+        {
+            // flatten data object in of an array
+            if (data.GetType() == typeof(JArray))
+                data = data[0];
+
+            // loop through all ribbon items in the "insert merge fields" menu
+            foreach (Control ribbonButton in ribbonItems)
+            {
+                // in case, the item is a drop-down, call ApplyMaskedString
+                // recursively with a new data object
+                if (ribbonButton is RibbonMenuButton)
+                {
+                    ApplyMaskedString(
+                        ((RibbonMenuButton)ribbonButton).DropDownItems,
+                        data[ribbonButton.Text]);
+                }
+                // in case it is a merge field insert button
+                else if (ribbonButton is RibbonButton)
+                {
+                    // and it is not a separator or title
+                    if (!ribbonButton.Name.StartsWith("TXITEM_"))
+                    {
+                        // get the data from the first data row
+                        var dataValue = data[ribbonButton.Text];
+
+                        if (dataValue == null)
+                            continue;
+
+                        if (dataValue.GetType() == typeof(JValue))
+                        { 
+                            // change the actual text
+                            ribbonButton.Text = dataValue.Value;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
